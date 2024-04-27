6fbc9a358e97ed570df106d4e9e694e8e4792c67
ksharperd <godleaveme233@gmail.com>
2024-04-28 03:00:15 +0800

    [TableConverter] collect table list from output.

--- TableConverter/Program.cs
+++ TableConverter/Program.cs
@@ -16,6 +16,7 @@ internal partial class Program
 
         var input = args[0];
         var output = args[1];
+        var renameOnly = (args.Length >= 3) && (args[2] == "--rename_only");
 
         if (!Directory.Exists(input))
         {
@@ -27,27 +28,59 @@ internal partial class Program
             Directory.CreateDirectory(output);
         }
 
-        var rawTableFiles = Directory.GetFiles(input, "*.tab.bytes", SearchOption.AllDirectories);
-        foreach (var fileName in rawTableFiles)
+        var inputSuffix = renameOnly ? ".tab" : ".tab.bytes";
+        var outputSuffix = ".tsv";
+
+        var targetTableDes = new Dictionary<string, string>();
+        foreach (var targetTableFile in Directory.GetFiles(output, "*.tsv", SearchOption.AllDirectories))
         {
-            var tempFileName = fileName.Replace(input, string.Empty).Replace(".tab.bytes", ".tsv");
-            if (tempFileName.StartsWith(Path.DirectorySeparatorChar))
+            targetTableDes.Add(Path.GetRelativePath(output, targetTableFile), new FileInfo(targetTableFile).Name.Replace(outputSuffix, string.Empty));
+        }
+        var targetTableParents = targetTableDes.Values;
+
+        foreach (var rawTableFile in Directory.GetFiles(input, renameOnly ? "*.tab" : "*.tab.bytes", SearchOption.AllDirectories))
+        {
+            var rawTableFileInfo = new FileInfo(rawTableFile);
+            var shortFileName = rawTableFileInfo.Name.Replace(inputSuffix, string.Empty);
+            var conflict = (shortFileName.Count(char.IsNumber) == shortFileName.Length) && targetTableParents.Any(parent => string.Equals(parent, Directory.GetParent(rawTableFile)!.Name, StringComparison.OrdinalIgnoreCase));
+
+            var fakeFileName = rawTableFile.Replace(rawTableFileInfo.Name, string.Empty);
+            fakeFileName = fakeFileName.Remove(fakeFileName.Length - 1) + outputSuffix;
+
+            IEnumerable<KeyValuePair<string, string>> results = [];
+            if (!(results = targetTableDes.Where(v => (conflict ? Path.GetRelativePath(input, fakeFileName) : rawTableFile.Replace(inputSuffix, outputSuffix)).EndsWith(v.Key, StringComparison.OrdinalIgnoreCase))).Any())
             {
-                tempFileName = tempFileName.Remove(0, 1);
+                continue;
             }
-            var newFilename = Path.Combine(output, tempFileName);
+
+            var result = results.First();
+            var targetFileName = result.Key;
+
+            Console.WriteLine($"Processing {targetFileName}");
+            if (conflict)
+            {
+                Console.WriteLine($"Conflict between {targetFileName} and {Path.GetRelativePath(input, rawTableFile)}");
+            }
+
+            var newFilename = Path.Combine(output, targetFileName);
             var newFileInfo = new FileInfo(newFilename);
             if (!Directory.Exists(newFileInfo.Directory!.FullName))
             {
                 Directory.CreateDirectory(newFileInfo.Directory!.FullName);
             }
 
-            var bytes = File.ReadAllBytes(fileName);
+            if (renameOnly)
+            {
+                File.Copy(rawTableFile, newFilename, true);
+                continue;
+            }
+
+            var bytes = File.ReadAllBytes(rawTableFile);
             var newBytes = new byte[bytes.Length - 128];
             Array.Copy(bytes, 128, newBytes, 0, newBytes.Length);
             File.WriteAllBytes(newFilename, newBytes);
 
-            Console.WriteLine($"Processed {fileName}");
+            Console.WriteLine($"Processed {rawTableFile}");
         }
     }
 
